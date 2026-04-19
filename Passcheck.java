b9a2c3687b5d5be95716c0c4ae6b4c26e5f4dd8e
sreenidhi-18 <dsreenidhi1514@gmail.com>
2026-04-19 10:37:06 +0530

    Create Passcheck.java

---
+++ Passcheck.java
@@ -0,0 +1,15 @@
+import java.util.regex.*;
+public class Passcheck {
+    public static boolean checkPassword(String password) 
+    {
+        String rules = "^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.[@#$%^&-+=()])(?=\\S$).{8,20}$";
+        Pattern pattern = Pattern.compile(rules);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkPassword("weak"));
+        System.out.println(checkPassword("DD")); 
+    }
+}
